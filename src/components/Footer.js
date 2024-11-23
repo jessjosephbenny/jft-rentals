856faf717402683d18dc84f91dc6adb4fd7f77da
Jess Joseph Benny <jessjosephbenny@gmail.com>
2024-11-22 19:15:35 -0500

    Added Gtag in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Footer = () => {
+
+  useEffect(()=>{
+    if (window.location.href.indexOf("/service") != -1) {
+      var x = 0
+      var myVar = setInterval(function () {
+        if (x == 0) {
+          if (
+            document.querySelector(".bg-white.p-4.rounded.shadow-lg") &&
+            document
+              .querySelector(".bg-white.p-4.rounded.shadow-lg")
+              .innerText.includes("Successfully")
+          ) {
+            window.gtag("event", "conversion", {
+              send_to: "AW-16635587622/H81bCLL46OIZEKbYu_w9",
+            })
+
+            clearInterval(myVar)
+            x = 1
+          }
+        }
+      }, 1000)
+    }
+  },[])
+
   return (
     <footer className="bg-gray-800 text-white text-center p-4">
       <div className="container mx-auto">
